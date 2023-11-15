9780161863085a2ebec5f5c248cde847233dd9b7
sebastian461 <sebastiano-461@hotmail.com>
2023-11-15 14:25:15 -0500

    Ejecutar multiples promesas

--- src/concepts/03-promises.js
+++ src/concepts/03-promises.js
@@ -6,7 +6,8 @@ import { heroes } from "../data/heroes";
  */
 export const promisesComponent = (element) => {
   console.log("promisesComponent");
-  const id = "5d86371fd55e2e2a30fe1cc3";
+  const id1 = "5d86371fd55e2e2a30fe1cc3";
+  const id2 = "5d86371f97c29d020f1e1f6d";
 
   const renderHeroe = (heroe) => {
     element.innerHTML = heroe.name;
@@ -19,9 +20,33 @@ export const promisesComponent = (element) => {
     `;
   };
 
-  findHero(id)
+  /* Manera en la que se ejecuta un promesa */
+  findHero(id1)
     .then((h) => renderHeroe(h))
     .catch((e) => renderError(e));
+
+  /* Manera en la que se ejecutan varias promesas que dependen entre si
+   evitando el promise hell (promesas dentro de promesas) */
+
+  const renderTwoHeroe = (heroe1, heroe2) => {
+    element.innerHTML = `${heroe1.name} </br> ${heroe2.name}`;
+  };
+
+  let heroe1;
+  findHero(id1)
+    .then((h1) => {
+      heroe1 = h1;
+      return findHero(id2);
+    })
+    .then((h2) => {
+      renderTwoHeroe(heroe1, h2);
+    })
+    .catch(renderError);
+
+  /* Manera en la que se ejecutan varias promesas que no dependen entre si */
+  Promise.all([findHero(id1), findHero(id2)])
+    .then(([h1, h2]) => renderTwoHeroe(h1, h2))
+    .catch(renderError);
 };
 
 /**
